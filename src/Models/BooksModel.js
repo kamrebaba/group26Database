cb6484207ef0d71f5aefd0efe0d81a5352734a76
Saquib <saquibparvez007@gmail.com>
2022-09-20 15:56:12 +0530

    UserModel and Controller,bookModel

---
+++ src/Models/BooksModel.js
@@ -0,0 +1,20 @@
+const mongoose= require('mongoose')
+const ObjectId= mongoose.Schema.Types.ObjectId
+
+const bookSchema= new mongoose.Schema({
+    
+        title: {type:String, required:true, unique:true},
+        excerpt: {type:String, required:true}, 
+        userId: {Type:ObjectId , ref:'User'},
+        ISBN: {type:String,required:true,unique:true},
+        category: {type:String, required:true},
+        subcategory: {type:[String], required:true},
+        reviews: {type:Number, default: 0, comment:" Holds number of reviews of this book"},
+        deletedAt: {type:Date}, 
+        isDeleted: {type:boolean, default: false},
+        releasedAt: {type:Date, required:true},
+        
+      
+},{timestamps:true}
+)
+model.excerpt= mongoose.model('Book',bookSchema)
